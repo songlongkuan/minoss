01bd34d597c150ec7f046cf68d6c1a21bb2b71ac
pencilso <5027954@qq.com>
2020-02-09 20:46:07 +0800

    Initialize commit

--- app/minoss-app-admin/src/main/java/io/javac/minoss/minossappadmin/VertxAccessController.java
+++ app/minoss-app-admin/src/main/java/io/javac/minoss/minossappadmin/VertxAccessController.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
